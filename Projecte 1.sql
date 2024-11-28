316d8548d4d628e0be50245d47b32f9c2257214b
KaranKodi2647 <karankodi26002@gmail.com>
2024-11-28 08:17:45 +0530

    new

---
+++ Projecte 1.sql
@@ -0,0 +1,11 @@
+SHOW DATABASES ;
+CREATE DATABASE collage ;
+SHOW DATABASES ;
+CREATE TABLE GANA (
+coleam int,STUDENT_NAME varchar (7),ROOALno int, fathername varchar (6), chacha int ,
+ primary key (ROOALno)
+) ;
+USE collage ;
+SHOW tables ;
+
+show tables;
